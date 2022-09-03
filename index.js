3ca6b2d930098f6edc33f215f25dc3bdb53b1d6e
Samy Fadel <samyfadel@google.com>
2022-09-03 19:47:47 +0200

    samy

--- index.js
+++ index.js
@@ -2,41 +2,10 @@ const express = require('express');
 const app = express();
 const port = 80;
 
-app.get('/', (req, res) => {
-    console.log("line6");
+app.get('/microservice2', (req, res) => {
     console.log(req.url);
     res.send('<h1>microservice2!</h1>');
 });
-
-app.get('/microservice2', (req, res) => {
-    console.log("line12");
-    console.log(req.url);
-    res.send('<h1>microservice2r!</h1>');
-});
-
-
-app.get('/v2', (req, res) => {
-    console.log("line13");
-    console.log(req.url);
-    res.send('<h1>microservicev22!</h1>');
-});
-
-app.get('v2', (req, res) => {
-    console.log(req.url);
-    res.send('<h1>microserviceV2!</h1>');
-});
-
-
-app.get('/microservicev2', (req, res) => {
-    console.log(req.url);
-    res.send('<h1>microservicex2!</h1>');
-});
-
-app.get('microservicev2', (req, res) => {
-    console.log(req.url);
-    res.send('<h1>microserviceX2!</h1>');
-});
-
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
